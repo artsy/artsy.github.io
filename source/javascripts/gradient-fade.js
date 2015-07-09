66b53f108220cfa72c08841e136902b0b35513f3
Brennan Moore <brennanmoore@gmail.com>
2015-07-08 22:42:45 -0400

    remove console.log. classic.

--- source/javascripts/gradient-fade.js
+++ source/javascripts/gradient-fade.js
@@ -3,7 +3,6 @@ $(function() {
     var $header = $('#banner');
     var updateBackgroundPosition = function(e) {
         scrollPosition = $window.scrollTop();
-        console.log(scrollPosition);
         $header.css({'background-position': "0px -" + scrollPosition + "px"});
     };
     $window.scroll(updateBackgroundPosition);
