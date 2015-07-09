0fa3ba164820e1bc8afbe2bbbaf54639b14f7450
Brennan Moore <brennanmoore@gmail.com>
2015-07-08 22:09:38 -0400

    add gradient

--- source/javascripts/gradient_fade.js
+++ source/javascripts/gradient_fade.js
@@ -1,15 +1,9 @@
 function init() {
-    // Create the listener function
+    var $window = $("window");
+    var $header = $('#banner');
     var updateLayout = function(e) {
-        var body = $("body");
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop;
-        if (distanceY > 120) {
-            body.css("background", "white");
-        } else {
-            var alpha = 1 - (distanceY / 120);
-            var gradient = "linear-gradient(209deg, rgba(217, 255, 255, "+alpha+"), rgba(252, 255, 246,  "+alpha+") 1300px, white 1900px)";
-            body.css("background", gradient);
-        }
+        scrollPosition = $window.scrollTop();
+        $header.css({'background-position': "0px -" + scrollPosition + "px"});
     };
     window.addEventListener('scroll', updateLayout, false);
 }
