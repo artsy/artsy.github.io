c7e07ee6d1997b7753b47ba2167f771eea1a9b49
Brennan Moore <brennanmoore@gmail.com>
2015-07-09 12:08:17 -0400

    fix for page bounce in safari

--- source/javascripts/gradient-fade.js
+++ source/javascripts/gradient-fade.js
@@ -3,7 +3,7 @@ $(function() {
     var $header = $('#banner');
     var updateBackgroundPosition = function(e) {
         scrollPosition = $window.scrollTop();
-        $header.css({'background-position': "0px -" + scrollPosition + "px"});
+        $header.css({'background-position': "0px -" + (scrollPosition - 100) + "px"});
     };
     $window.scroll(updateBackgroundPosition);
 });
