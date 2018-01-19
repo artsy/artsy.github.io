b427f3c642cc588c281b3b3d555dc56a24eb55a4
Jon Allured <jon.allured@gmail.com>
2018-01-19 16:56:15 -0600

    Add a category list page

--- _plugins/category_generator.rb
+++ _plugins/category_generator.rb
@@ -19,9 +19,27 @@
 # - category_title_prefix: The string used before the category name in the page title (default is
 #                          'Category: ').
 
-PostCategory = Struct.new(:name, :dir)
+PostCategory = Struct.new(:name, :dir) do
+  def to_liquid
+    { 'name' => name, 'dir' => dir }
+  end
+end
 
 module Jekyll
+
+  class PostCategoryListPage < Page
+    def initialize(site, base, post_categories)
+      @site = site
+      @base = base
+      @dir = "/blog/categories"
+      @name = "index.html"
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'category_list_page.html')
+      self.data['post_categories'] = post_categories
+      self.data['title'] = "Post Categories"
+      self.data['description'] = "Post Categories"
+    end
+  end
 
   # The CategoryIndex class creates a single category page for the specified category.
   class CategoryIndex < Page
@@ -109,10 +127,12 @@ module Jekyll
         dir = self.config['category_dir'] || 'categories'
 
         post_categories = self.categories.keys.map do |category|
-          category_dir = File.join(dir, category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase)
+          normalized_category_name = category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-')
+          category_dir = File.join(dir, normalized_category_name.downcase)
           PostCategory.new(category, category_dir)
         end
 
+        self.write_category_list_page(post_categories)
         post_categories.each do |post_category|
           self.write_category_index(post_category.dir, post_category.name)
         end
@@ -123,6 +143,13 @@ module Jekyll
       end
     end
 
+
+    def write_category_list_page(post_categories)
+      page = PostCategoryListPage.new(self, self.source, post_categories)
+      page.render(self.layouts, site_payload)
+      page.write(self.dest)
+      self.pages << page
+    end
   end
 
 
